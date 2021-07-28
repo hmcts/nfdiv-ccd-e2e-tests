4b79cd60716985d584e0c749e32df9c8f2e3e3fd
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-07-28 10:41:21 +0100

    Update for verifyCaseStateAndEvents test.

--- tests/nfdiv/verifyCaseStateAndEvents.test.js
+++ tests/nfdiv/verifyCaseStateAndEvents.test.js
@@ -18,23 +18,19 @@ Scenario('Create NFD Case in CCD and verify the states Draft, AwaitingHWF and Su
   caseId = await createNFDCaseInCcd('data/ccd-nfdiv-draft-case.json');
   console.log( '.....caseCreated in CCD and caseId is...... ' + caseId);
 
-   // const awaitingHWF = await updateNFDCaseInCcd(user.SOLS,caseId, events.SOLICITOR_SUBMIT_APPLICATION,'data/ccd-nfd-draft-to-submitted-state.json');
-   // verifyState(awaitingHWF, states.AWAITING_HWF);
-   //
-   // const hwfAccepted = await updateNFDCaseInCcd(user.CW,caseId, events.CASEWORKER_HWF_APPLICATION_ACCEPTED,'data/ccd-nfd-hwf-accepted.json');
-   // verifyState(hwfAccepted, states.SUBMITTTED);
-   //
-   // const issued = await updateNFDCaseInCcd(user.CW,caseId, events.ISSUED_FROM_SUBMITTED,'data/ccd-update-marriage-data.json');
-   // verifyState(issued, states.ISSUED);
-   //
-   // const issueAosPack = await updateNFDCaseInCcd(user.CA,caseId, events.ISSUE_AOS,'data/ccd-update-marriage-data.json');
-   // verifyState(issueAosPack, states.AOS_AWAITING);
+  const awaitingHWF = await updateNFDCaseInCcd(user.SOLS,caseId, events.SOLICITOR_SUBMIT_APPLICATION,'data/ccd-nfd-draft-to-submitted-state.json');
+  verifyState(awaitingHWF, states.AWAITING_HWF);
 
-  // share teh case as a RespAdminstrator ....
+   const hwfAccepted = await updateNFDCaseInCcd(user.CW,caseId, events.CASEWORKER_HWF_APPLICATION_ACCEPTED,'data/ccd-nfd-hwf-accepted.json');
+   verifyState(hwfAccepted, states.SUBMITTTED);
+
+   const issueAosPack = await updateNFDCaseInCcd(user.CA,caseId, events.ISSUED_FROM_SUBMITTED,'data/ccd-update-marriage-data.json');
+   verifyState(issueAosPack, states.AOS_AWAITING);
+
+  // share the case as a RespAdminstrator ....
 
   // await I.amOnHomePage();
   // await I.login(testconfigMO.TestEnvRespondentSolsAdminUser, testconfigMO.TestEnvRespondentSolsAdminPassword);
-
 
   //const startAOS = await updateNFDCaseInCcd(user.RS, caseId, events.AOS_AWAITING_TO_AOS_DRAFTED,'data/ccd-ndf-aos-awaiting-to-aos-drafted.json');
   //verifyState(startAOS, states.AOS_DRAFTED);
