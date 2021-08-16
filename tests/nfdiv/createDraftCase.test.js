a1c968548e2d4f52aba9be0dc94e32f617e703f0
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-08-16 15:57:51 +0100

    Create Draft Case

---
+++ tests/nfdiv/createDraftCase.test.js
@@ -0,0 +1,10 @@
+const {createNFDCaseInCcd} = require('../../helpers/utils');
+const testconfig = require('./../config');
+
+let caseId;
+Feature('NFD  Case Create ');
+
+Scenario('NFD - Sole Divorce Case created in CCD in Draft State', async function (I) {
+   caseId = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-case.json');
+   console.log( '.....caseCreated in CCD and caseId is...... ' + caseId);
+}).retry(testconfig.TestRetryScenarios);
