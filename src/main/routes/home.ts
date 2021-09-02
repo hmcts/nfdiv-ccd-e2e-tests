aaa23c62d46de6e7f249aa5956517b913b820916
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-09-02 13:52:35 +0100

    removing references to express-js-template

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,9 +1,9 @@
-import { Application } from 'express';
-
-export default function(app: Application): void {
-
-  app.get('/', (req, res) => {
-    res.render('home');
-  });
-
-}
+// import { Application } from 'express';
+//
+// export default function(app: Application): void {
+//
+//   app.get('/', (req, res) => {
+//     res.render('home');
+//   });
+//
+// }
