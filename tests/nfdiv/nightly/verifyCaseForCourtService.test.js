49aef7b7371daf689bf2acb9d4d8b4b9214664eb
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-09-09 22:55:32 +0100

    Attempt to move both tests to nightly folder

---
+++ tests/nfdiv/nightly/verifyCaseForCourtService.test.js
@@ -0,0 +1,34 @@
+const {createNFDCaseInCcd,updateNFDCaseInCcd,updateRoleForCase,shareCaseToRespondentSolicitor} = require('../../../helpers/utils');
+const { states, events , user} = require('../../../common/constants');
+const assert = require('assert');
+const testConfig = require('./../../config');
+
+
+const verifyState = (eventResponse, state) => {
+  assert.strictEqual(JSON.parse(eventResponse).state, state);
+};
+
+let caseNumber;
+
+Feature('NFD Case - Court Service');
+
+Scenario('NFD - Sole NFD CourtService case and verify State and Events', async function (I) {
+
+  caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-case.json');
+  console.log( '.....caseCreated in CCD , caseId is ==  ' + caseNumber);
+
+  // courtService
+  const awaitingHWF = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SOLICITOR_SUBMIT_APPLICATION,'data/ccd-nfd-draft-accept-sot-courtservice.json');
+  verifyState(awaitingHWF, states.AWAITING_HWF);
+
+  const hwfAccepted = await updateNFDCaseInCcd(user.CW,caseNumber, events.CASEWORKER_HWF_APPLICATION_ACCEPTED,'data/ccd-nfd-hwf-accepted.json');
+  verifyState(hwfAccepted, states.SUBMITTTED);
+
+  const issueAosPack = await updateNFDCaseInCcd(user.CA,caseNumber, events.ISSUED_FROM_SUBMITTED,'data/ccd-update-place-of-marriage.json');
+  verifyState(issueAosPack, states.AOS_AWAITING);
+
+  const shareACase = await updateRoleForCase(user.CA,caseNumber,'APPTWOSOLICITOR');
+
+  //const caseAvailableToRespondentSolicitor = await shareCaseToRespondentSolicitor(user.RSA,caseId);
+
+}).retry(testConfig.TestRetryScenarios);
