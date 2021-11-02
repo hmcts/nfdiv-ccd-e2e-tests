5d7d36ae7b3110db9d28d5f7e0a4d6a31250453a
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-11-02 17:32:15 +0000

    fix verifyBulkCase.test.js

--- tests/nfdiv/nightly/verifyBulkCase.test.js
+++ tests/nfdiv/nightly/verifyBulkCase.test.js
@@ -9,7 +9,7 @@ const verifyState = (eventResponse, state) => {
 
 let caseNumber;
 
-Feature('NFD - Move Case to a Bulk Pack');
+Feature('NFD - Create a single Case and move it to a Bulk List');
 
 // TODO Test works locally but fails on pipeline . This is because of the ShareACase uses http instead of https.
 // Pipeline expects https . HTTP works when tests are run locally ,but they fail on pipeline.
@@ -45,42 +45,26 @@ xScenario('NFD - Verify Bulk Case ', async function (I) {
   // To Move case from 20WeekHolding to AwaitingConditionalOrder  .... Call CCD API to mimic the cron job.
   // and set the dueDate to null ..(See SystemProgressHeldCasesTask in nfdiv-case-api)
 
-  console.log('~~~~~~~~~~~~ about to Call the moveFromHoldingToAwaitingCO ..~~~~~ ');
   const awaitingConditionalOrder = await moveFromHoldingToAwaitingCO('data/await-co-data.json',caseNumber);
   assert.strictEqual(JSON.parse(awaitingConditionalOrder).state, 'AwaitingConditionalOrder');
 
+  // Draft CO
   const draftConditionalOrder = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SOLS_DRAFT_CO,'data/ccd-draft-co.json');
   verifyState(draftConditionalOrder, stateDisplayName.CONDITIONAL_ORDER_DRAFTED);
 
-  const submitConditionalOrder = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SUBMIT_CO,'data/ccd-submit-co.json');
+  // Submit CO
+  const awaitingLegalAdvisorReferral = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SUBMIT_CO,'data/ccd-submit-co.json');
 
-  verifyState(submitConditionalOrder, states.AWAITING_LEGAL_ADVISOR_REFERRAL);
+  verifyState(awaitingLegalAdvisorReferral, states.AWAITING_LEGAL_ADVISOR_REFERRAL);
 
-  // get case ready for bulk
-  await I.amOnHomePage();
-  await I.wait(5);
-  await I.login(testConfig.TestEnvCourtAdminUser, testConfig.TestEnvCourtAdminPassword);
-  await I.wait(3);
-  await I.filterByCaseId(caseNumber);
-  await I.amOnPage('/case-details/' + caseNumber);
-  await I.wait(5);
-  await I.see('Awaiting legal advisor referral');
-  //await I.see('Application issued');
-  await I.checkNextStepForEvent('Link with bulk case');
-  await I.linkWithBulkCase(caseNumber);
-  await I.wait(5);
-  await I.see('Awaiting legal advisor referral');
-  await I.see('Link with bulk case');
-  await I.signOut();
-
-  // BulkCase with just one CaseParty reference . Purely for e2e purpose Only and to enable testing of the
-  // Pages that follow it.
+  //Note:Important: BulkCase with just ONE CaseParty reference . Purely for e2e purpose Only and to enable testing of the Pages that follow it.
   const bulkCaseReferenceId = await moveCaseToBulk('data/bulk-case-data.json',caseNumber);
 
+  // Login as CA with CaseType as 'NO_FAULT_DIVORCE_BulkAction' and check for BulkCase Created
+  await I.wait(5);
   await I.amOnHomePage();
-  await I.wait(8);
   await I.login(testConfig.TestEnvCourtAdminUser, testConfig.TestEnvCourtAdminPassword);
-  await I.wait(8);
+  await I.wait(5);
   await I.filterByBulkCaseReference(bulkCaseReferenceId);
   await I.amOnPage('/case-details/' + bulkCaseReferenceId);
   await I.wait(5);
