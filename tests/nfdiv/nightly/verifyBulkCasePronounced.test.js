5629b41e46666136d9fd2ca2d1100ed90bf610b6
Annika <annika.moorthy@solirius.com>
2021-11-04 11:58:23 +0000

    updated test for bulkcase pronounced

--- tests/nfdiv/nightly/verifyBulkCasePronounced.test.js
+++ tests/nfdiv/nightly/verifyBulkCasePronounced.test.js
@@ -9,11 +9,11 @@ const verifyState = (eventResponse, state) => {
 
 let caseNumber;
 
-Feature('NFD - Create a single Case and move it to a Bulk Listed');
+Feature('NFD - Create a single Case and move it to bulk case pronounced state');
 
 
 
-Scenario('NFD - Verify Bulk Case & print for pronouncement event', async function (I) {
+Scenario('NFD - Verify Bulk case pronounced', async function (I) {
 
   caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-bulk-case.json');
   console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
