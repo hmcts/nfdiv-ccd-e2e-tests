4f5a8c1d0c83152f325d9350ffe9952d23207851
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-11-29 21:20:29 +0000

    fixes on the aoS portion of the script.

--- tests/nfdiv/nightly/verifyHoldingToConditionalOrder.test.js
+++ tests/nfdiv/nightly/verifyHoldingToConditionalOrder.test.js
@@ -1,7 +1,8 @@
-const {createNFDCaseInCcd,updateNFDCaseInCcd,updateRoleForCase,shareCaseToRespondentSolicitor,moveFromHoldingToAwaitingCO} = require('../../../helpers/utils');
-const { states, events , eventDisplayName,user} = require('../../../common/constants');
+const {createNFDCaseInCcd,updateNFDCaseInCcd,updateRoleForCase,shareCaseToRespondentSolicitor,
+  moveFromHoldingToAwaitingCO} = require('../../../helpers/utils');
+const {states, events , eventDisplayName,user} = require('../../../common/constants');
 const assert = require('assert');
-const testconfig = require('./../../config');
+const testConfig = require('./../../config');
 
 const verifyState = (eventResponse, state) => {
   assert.strictEqual(JSON.parse(eventResponse).state, state);
@@ -33,10 +34,15 @@ Scenario('NFD - Share a Case and Draft AoS', async function (I) {
 
   console.log('~~~~~~~~~ Case with Id ' + caseNumber +' has been SUCCESSFULLY SHARED  to Respondent Solicitior');
 
-  // TODO Draft ,Update and Submit AOS should be made into a script here as the Page Flows are already tested as part of the shareACaseAndDraftUpdateSubmitAoS.test.js
+  // TODO
+  // Draft ,Update and Submit AOS UI page flows are superflous.
+  // They should be made into a script here as the Page Flows are already
+  // tested as part of the shareACaseAndDraftUpdateSubmitAoS.test.js
+
   //Draft AoS
   await I.amOnHomePage();
-  await I.login(testconfig.TestEnvRespondentSolUser, testconfig.TestEnvRespondentSolPassword);
+  await I.wait(8);
+  await I.login(testConfig.TestEnvRespondentSolUser, testConfig.TestEnvRespondentSolPassword);
   await I.filterByCaseId(caseNumber);
   await I.amOnPage('/case-details/' + caseNumber);
 
@@ -44,38 +50,37 @@ Scenario('NFD - Share a Case and Draft AoS', async function (I) {
   await I.draftAosContactDetails();
   await I.draftAoSReview(caseNumber);
   await I.draftAoSDoYouAgree(caseNumber);
+
   await I.draftAoSDoYouAgreeCourts(caseNumber);
   await I.draftAoSAnyOtherLegalProceedings(caseNumber);
   await I.draftAosCheckYourAnswers(caseNumber);
-  await I.see('AoS drafted');
 
-  // Update AoS
+  //Update AoS
   await I.checkNextStepForEvent(eventDisplayName.UPDATE_AOS);
-  await I.aosUpdateReviewApplicant1Application(caseNumber);
+  await I.updateAoSConfirmContactDetails(caseNumber);
   await I.aosUpdateReviewApplicant1ApplicationRes(caseNumber);
   await I.aosUpdateDispute(caseNumber);
-  await I.aosUpdateDoYouAgreeCourts(caseNumber);
   await I.aosUpdateLegal(caseNumber);
+  await I.aosUpdateLegalProceedings(caseNumber);
   await I.aosUpdateCYA(caseNumber);
-  await I.see('AoS drafted');
 
-  //TODO Uncomment and test .
-
+  // Submit AoS
+  await I.wait(2);
+  await I.amOnPage('/cases/case-details/' + caseNumber);
+  await I.wait(5);
   await I.checkNextStepForEvent(eventDisplayName.SUBMIT_AOS);
   await I.submitAosSOT(caseNumber);
   await I.submitAOSSotSolicitorDetails(caseNumber);
   await I.submitAosCYA(caseNumber);
-
-
-  await I.wait(10);
+  await I.wait(5);
+  await I.amOnPage('/case-details/' + caseNumber);
   await I.see('20 week holding period');
+
   await I.signOut();
   await I.wait(3);
 
   // To Move case from 20WeekHolding to AwaitingConditionalOrder  .... Call CCD API to mimic the cron job.
   // and set the dueDate to null ..See SystemProgressHeldCasesTask.java  in nfdiv-case-api
-
   const response = await moveFromHoldingToAwaitingCO('data/await-co-data.json',caseNumber);
   assert.strictEqual(JSON.parse(response).state, 'AwaitingConditionalOrder');
-
-}).retry(testconfig.TestRetryScenarios);
+}).retry(testConfig.TestRetryScenarios);
