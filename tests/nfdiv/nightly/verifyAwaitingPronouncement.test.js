002e0c793bde65c68e11ec94f6fdff43dbd1457e
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-12-06 14:45:34 +0000

    Fixing hearingDate in Future and event Name 'Apply:divorce or dissolution'

--- tests/nfdiv/nightly/verifyAwaitingPronouncement.test.js
+++ tests/nfdiv/nightly/verifyAwaitingPronouncement.test.js
@@ -39,8 +39,8 @@ Scenario('NFD - Move Case upto Listed;Awaiting Pronouncement', async function (I
   const submitAoS = await updateNFDCaseInCcd(user.RS,caseNumber, events.SUBMIT_AOS,'data/ccd-submit-aos.json');
   verifyState(submitAoS, states.HOLDING);
 
-  // To Move case from 20WeekHolding to AwaitingConditionalOrder  .... Call CCD API to mimic the cron job.
-  // and set the dueDate to null ..See SystemProgressHeldCasesTask in nfdiv-case-api
+  //To Move case from 20WeekHolding to AwaitingConditionalOrder  .... Call CCD API to mimic the cron job.
+  //and set the dueDate to null ..See SystemProgressHeldCasesTask in nfdiv-case-api
 
   console.log('~~~~~~~~~~~~ about to Call the moveFromHoldingToAwaitingCO ..~~~~~ ');
   const awaitingConditionalOrder = await moveFromHoldingToAwaitingCO('data/await-co-data.json',caseNumber);
