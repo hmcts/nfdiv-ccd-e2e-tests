84e4074dd8811212b866385380c1f76a94d171ee
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-12-10 11:46:25 +0000

    adding test for confidentialApplicant tab.

---
+++ tests/nfdiv/nightly/applicant1ContactDetailsNotConfidentialScenario.test.js
@@ -0,0 +1,31 @@
+const {createNFDCaseInCcd,updateNFDCaseInCcd,updateRoleForCase,shareCaseToRespondentSolicitor,moveFromHoldingToAwaitingCO,moveCaseToBulk} = require('../../../helpers/utils');
+const { states, events , user, stateDisplayName} = require('../../../common/constants');
+const assert = require('assert');
+const testConfig = require('./../../config');
+
+const verifyState = (eventResponse, state) => {
+  assert.strictEqual(JSON.parse(eventResponse).state, state);
+};
+
+let caseNumber;
+
+Feature('NFD - Confidential Applicant');
+
+//TODO Test  Assert ConfidentialDocuments Tab once this test has passed.
+
+Scenario('NFD - Assert Data in the Confidential Documents Tab - App1 Represented and app1ContactDetails Not Confidential ',
+  async function (I) {
+
+  //"applicant1KeepContactDetailsConfidential": "No", in the  File below.
+  caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-app1-details-not-confidential-case.json');
+  console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
+
+  //SoT solServiceMethod == courtService
+  const awaitingHWF = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SOLICITOR_SUBMIT_APPLICATION,'data/ccd-nfd-draft-sot-courtservice.json');
+  verifyState(awaitingHWF, states.AWAITING_HWF);
+
+  const hwfAccepted = await updateNFDCaseInCcd(user.CA,caseNumber, events.CASEWORKER_HWF_APPLICATION_ACCEPTED,'data/ccd-nfd-hwf-accepted.json');
+  verifyState(hwfAccepted, states.SUBMITTTED);
+
+}).retry(testConfig.TestRetryScenarios);
+
---
+++ tests/nfdiv/nightly/updateFOEligibleDate.test.js
@@ -0,0 +1,23 @@
+const {createNFDCaseInCcd,updateNFDCaseInCcd,updateRoleForCase,shareCaseToRespondentSolicitor,moveFromHoldingToAwaitingCO,moveCaseToBulk} = require('../../../helpers/utils');
+const { states, events , user, stateDisplayName} = require('../../../common/constants');
+const assert = require('assert');
+const testConfig = require('./../../config');
+
+const verifyState = (eventResponse, state) => {
+  assert.strictEqual(JSON.parse(eventResponse).state, state);
+};
+
+let caseNumber;
+
+xFeature('NFD - FO EligibleDate');
+
+xScenario('NFD - Verify Bulk Case ', async function (I) {
+
+  // caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-bulk-case.json');
+  // console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
+
+  await updateNFDCaseInCcd(user.CA,'1638983039263228', "system-progress-case-awaiting-final-order",'data/ccd-nfd-eligibledate.json');
+
+
+}).retry(testConfig.TestRetryScenarios);
+
