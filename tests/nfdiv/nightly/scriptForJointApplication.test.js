ba738d85c49a29dfc93a63d7b68ed7013bb296a9
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2022-01-28 15:35:03 +0000

    Adding method for getCaseDetails given a caseId.

--- tests/nfdiv/nightly/scriptForJointApplication.test.js
+++ tests/nfdiv/nightly/scriptForJointApplication.test.js
@@ -1,6 +1,6 @@
 const {paymentType,yesorno,divorceOrDissolution, states} = require('../../../common/constants');
 const testconfig = require('./../../config');
-const {createNFDCaseInCcd} = require('../../../helpers/utils');
+const {createNFDCaseInCcd,getCaseDetailsFor} = require('../../../helpers/utils');
 
 let caseNumber;
 
@@ -10,6 +10,8 @@ Scenario('Solicitor Create Joint Divorce Draft Case', async (I) => {
 
   caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-joint-draft-case.json');
   console.log( '..... Joint Case Created in CCD , CaseNumber is ==  ' + caseNumber);
+
+  console.log('GET Case Details ... returns ...' + await getCaseDetailsFor(caseNumber));
 
 }).retry(testconfig.TestRetryScenarios);
 
