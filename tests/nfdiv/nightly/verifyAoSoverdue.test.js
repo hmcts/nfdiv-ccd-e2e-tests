296c52df53dcc28afa944df2ce7dd63b12c97e9a
Annika <annika.moorthy@solirius.com>
2022-01-31 17:18:58 +0000

    aos overdue cron from aos drafted test

--- tests/nfdiv/nightly/verifyAoSoverdue.test.js
+++ tests/nfdiv/nightly/verifyAoSoverdue.test.js
@@ -13,7 +13,7 @@ let caseNumber;
 
 Feature('NFD  - AoS Overdue');
 
-Scenario('NFD - Move case into AoS Overdue', async function (I) {
+Scenario('NFD - Move case into AoS Overdue from Aos Awaiting', async function (I) {
 
   caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-case.json');
   console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
@@ -34,6 +34,37 @@ Scenario('NFD - Move case into AoS Overdue', async function (I) {
   assert.strictEqual(JSON.parse(caseSharedToRespSolicitor).status_message, 'Roles [APPTWOSOLICITOR] from the organisation policies successfully assigned to the assignee.');
 
   console.log('~~~~~~~~~ Case with Id ' + caseNumber +' has been SUCCESSFULLY SHARED  to Respondent Solicitior'); //at Awaiting AoS
+
+  const response = await updateAoSToAoSOverdue(caseNumber,'system-progress-to-aos-overdue','data/aos-overdue.json');
+  verifyState(response, states.AOS_OVERDUE);
+
+
+}).retry(testconfig.TestRetryScenarios);
+
+Scenario('NFD - Move case into AoS Overdue from Aos Drafted', async function (I) {
+
+  caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-case.json');
+  console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
+
+  // SoT solServiceMethod == courtService
+  const awaitingHWF = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SOLICITOR_SUBMIT_APPLICATION,'data/ccd-nfd-draft-sot-courtservice.json');
+  verifyState(awaitingHWF, states.AWAITING_HWF);
+
+  const hwfAccepted = await updateNFDCaseInCcd(user.CA,caseNumber, events.CASEWORKER_HWF_APPLICATION_ACCEPTED,'data/ccd-nfd-hwf-accepted.json');
+  verifyState(hwfAccepted, states.SUBMITTTED);
+
+  const awaitingService = await updateNFDCaseInCcd(user.CA,caseNumber, events.ISSUED_FROM_SUBMITTED,'data/ccd-update-place-of-marriage.json');
+  verifyState(awaitingService, states.AOS_AWAITING);
+
+  const shareACase = await updateRoleForCase(user.RS, caseNumber, 'APPTWOSOLICITOR');
+
+  const caseSharedToRespSolicitor = await shareCaseToRespondentSolicitor(user.RSA,caseNumber);
+  assert.strictEqual(JSON.parse(caseSharedToRespSolicitor).status_message, 'Roles [APPTWOSOLICITOR] from the organisation policies successfully assigned to the assignee.');
+
+  console.log('~~~~~~~~~ Case with Id ' + caseNumber +' has been SUCCESSFULLY SHARED  to Respondent Solicitior'); //at Awaiting AoS
+
+  const draftAoS = await updateNFDCaseInCcd(user.RS,caseNumber, events.DRAFT_AOS,'data/ccd-draft-aos.json');
+  verifyState(draftAoS, states.AOS_DRAFTED);
 
   const response = await updateAoSToAoSOverdue(caseNumber,'system-progress-to-aos-overdue','data/aos-overdue.json');
   verifyState(response, states.AOS_OVERDUE);
