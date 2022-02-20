3b678068a36361ef63771b6b706523172cd50a99
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2022-02-20 14:49:56 +0000

    Script For Joint Journey - upto Holding

--- tests/nfdiv/nightly/scriptForJointApplication.test.js
+++ tests/nfdiv/nightly/scriptForJointApplication.test.js
@@ -1,13 +1,18 @@
-const {paymentType,yesorno,divorceOrDissolution, states} = require('../../../common/constants');
+const {states, user, events} = require('../../../common/constants');
 const testConfig = require('./../../config');
-const {createNFDCaseInCcd,getCaseDetailsFor} = require('../../../helpers/utils');
+const {createNFDCaseInCcd,getCaseDetailsFor, updateNFDCaseInCcd, updateRoleForCase, shareCaseToRespondentSolicitor} = require('../../../helpers/utils');
 const assert = require('assert');
+const testconfig = require('./../../config');
 
 let caseNumber;
 
+const verifyState = (eventResponse, state) => {
+  assert.strictEqual(JSON.parse(eventResponse).state, state);
+};
+
 Feature('Joint Application ');
 
-Scenario('Solicitor Create Joint Divorce Draft Case', async (I) => {
+Scenario('Script - XUI Joint Divorce Case - upto Holding State', async (I) => {
 
   caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-joint-draft-case.json');
   console.log( '..... Citizen Case Created in CCD and the  CaseNumber is ==  ' + caseNumber);
@@ -20,7 +25,60 @@ Scenario('Solicitor Create Joint Divorce Draft Case', async (I) => {
   assert.strictEqual(orgName,'NFD Solicitor Organisation');
   assert.strictEqual(docType,'correspondence');
 
+  // Invite Applicant 2
+  const awaitApplicant2Response = await updateNFDCaseInCcd(user.SOLS,caseNumber, 'invite-applicant2','data/ccd-empty-payload.json');
+  verifyState(awaitApplicant2Response,'AwaitingApplicant2Response');
+
+  // Update Role For Case
+  const app2SolicitorRoleForCase = await updateRoleForCase(user.RS,caseNumber,'APPTWOSOLICITOR');
+
+  // Share Case To Respondent Solicitor
+  const caseSharedToRespSolicitor = await shareCaseToRespondentSolicitor(user.RSA,caseNumber);
+  assert.strictEqual(JSON.parse(caseSharedToRespSolicitor).status_message, 'Roles [APPTWOSOLICITOR] from the organisation policies successfully assigned to the assignee.');
+
+  // login as Respondent Solicitor and 'Submit Joint Application'
+  const respSolsSubmitApplication = await updateNFDCaseInCcd(user.RS,caseNumber, 'solicitor-submit-joint-application','data/ccd-nfd-resp-sols-submit-application.json');
+  verifyState(respSolsSubmitApplication,'AwaitingApplicant2Response');
+
+  // async CCD process that will move the case to 'applicant2-approve'
+  await I.wait(8);
+
+  // TODO , not able to script the 'solicitor-submit-joint-application' as App1 Solicitor.
+  // Going down the UI Route , but it would be good to have this scripted as well.
+
+  //let caseResponseDetails = getCaseDetailsFor(caseNumber);
+  //verifyState(caseResponseDetails,'Applicant2Approved');
+  //  Login as Appicant-1-Solicitor  (TEST_SOLICITOR@mailinator.com), AND Sign and Submit .
+  // const applicant1SolSignAndSubmit = await updateNFDCaseInCcd(user.SOLS,caseNumber, 'solicitor-submit-joint-application',
+  //             'data/ccd-nfd-app1-solicitor-submit-application.json');
+  //verifyState(applicant1SolSignAndSubmit,states.AWAITING_HWF);
+
+  await I.amOnHomePage();
+  await I.login(testconfig.TestEnvSolUser, testconfig.TestEnvSolPassword);
+  await I.wait(10);
+  await I.shouldBeOnCaseListPage();
+
+  await I.wait(5);
+  await I.amOnPage('/case-details/' + caseNumber);
+
+  await I.checkNextStepForEvent('Sign and submit');
+  await I.submitSignAndSubmit();
+
+  await I.paymentWithPbaAccount();
+
+  await I.fillPba();
+
+  await I.PbaAccountOrderSummary();
+
+  await I.caseCheckYourAnswersPageFormAndSubmit();
+
+  await I.signOut();
+
+  console.log('~~~~~~~~~~~~~  DONE ....Solicitor sign and submit Joint Application ~~~~~~~');
+
+  await I.wait(5);
+
+  const awaitingService = await updateNFDCaseInCcd(user.CA,caseNumber, events.ISSUED_FROM_SUBMITTED,'data/ccd-update-place-of-marriage.json');
+  verifyState(awaitingService, states.HOLDING);
 
 }).retry(testConfig.TestRetryScenarios);
-
-
