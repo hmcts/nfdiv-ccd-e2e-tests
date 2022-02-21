f4088dbed934e76f9643d8960d237a2bc581bebd
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2022-02-21 20:01:42 +0000

    Added Test for Service Decision (CP journey )  and updated test for applicationTabData

--- tabs/nfdiv/validateApplicationTabData.js
+++ tabs/nfdiv/validateApplicationTabData.js
@@ -1,122 +1,68 @@
 const I = actor();
 const { firstLetterToCaps, formatDateToCcdDisplayDate } = require('../../helpers/utils');
-const labels = require('../../data/tab-fields/nfdiv/application.json');
+const labels = require('../../data/tab-fields/nfdiv/application-tab-fields.json');
+
 const commonFields = require('../../data/tab-fields/nfdiv/commonFields.json');
 const { reasonsForDivorce } = require('../../common/constants');
 
-
 module.exports = async (reason, verifyContent) => {
   await I.clickTab(labels.name);
+
+  // Application Tab.
   await I.see(labels.whenApplicationCreated);
   await I.see(labels.whenApplicationSubmitted);
   await I.see(labels.whenApplicationIssued);
+  await I.see(labels.applicationType);
+  await I.see(verifyContent.applicationType);
+  await I.see(labels.divorceOrCivil);
+  await I.see(verifyContent.labelContentUnionTypeUC);
+  await I.see(labels.rdc);
 
-  await I.see(labels.rdc); //"Courts and Tribunals Service Centre",
-  await I.see(verifyContent.applicant1FirstName);
-  // await I.see(labels.petitionerLName);
-  // await I.see(verifyContent.D8PetitionerLastName);
-  //
+  await I.see(labels.theApplicant);
+  await I.see(labels.firstName);
+  await I.see(labels.lastName);
+  await I.see(labels.middleName);
 
-  // await I.clickTab(labels.name);
-  // await I.see(labels.rdc);
-  // await I.see(labels.courtName); //"Courts and Tribunals Service Centre",
-  // await I.see(labels.petitionerFName);
-  // await I.see(verifyContent.D8PetitionerFirstName);
-  // await I.see(labels.petitionerLName);
-  // await I.see(verifyContent.D8PetitionerLastName);
-  // await I.see(labels.petionerGender);
-  // await I.see(firstLetterToCaps(verifyContent.D8InferredPetitionerGender));
-  // await I.see(labels.petitionerChangedName);
-  // await I.see(firstLetterToCaps(verifyContent.D8PetitionerNameDifferentToMarriageCert));
-  // await I.see(labels.placeOfMarriage);
-  // await I.see(verifyContent.D8MarriagePlaceOfMarriage);
-  // await I.see(labels.WhoPetitionerDivorcing);
-  // await I.see(firstLetterToCaps(verifyContent.D8DivorceWho));
-  // await I.see(labels.sameSexCouple);
-  // await I.see(firstLetterToCaps(verifyContent.D8MarriageIsSameSexCouple));
-  // await I.see(labels.petitionerFactsStated);
-  // await I.see(commonFields.yes);
-  // await I.see(labels.anyPreviousProceedings);
-  // await I.see(firstLetterToCaps(verifyContent.D8LegalProceedings));
-  //
-  // //Respondent details
-  // await I.see(labels.respondentFName);
-  // await I.see(verifyContent.D8RespondentFirstName);
-  // await I.see(labels.respondentLName);
-  // await I.see(verifyContent.D8RespondentLastName);
-  // await I.see(labels.respHomeAddress);
-  // await I.see(verifyContent.D8DerivedRespondentHomeAddress);
-  // await I.see(labels.respondentSerAdd);
-  // await I.see(verifyContent.D8DerivedRespondentCorrespondenceAddr);
-  // await I.see(labels.respondentConsentEmailComm);
-  // await I.see(commonFields.yes);
-  // await I.see(labels.respGender);
-  // await I.see(firstLetterToCaps(verifyContent.D8InferredRespondentGender));
-  //
-  // await I.see(labels.wishToApplyForFinancialOrder);
-  // await I.see( firstLetterToCaps(verifyContent.D8FinancialOrder));
-  // if(reasonsForDivorce.ADULTERYDISPLAY === reason) {
-  //   await I.see(labels.applyForClaimCosts);
-  //   await I.see(firstLetterToCaps(verifyContent.D8DivorceCostsClaim));
-  // }
-  // if(reasonsForDivorce.BEHAVIOURDISPLAY === reason) {
-  //   await I.see(labels.whoTheFinancialOrder);
-  //   await I.see(verifyContent.D8FinancialOrderFor[0]);
-  //   await I.see(labels.applyForClaimCosts);
-  //   await I.see(firstLetterToCaps(verifyContent.D8DivorceCostsClaim));
-  //   await I.see(labels.claimCostsFrom);
-  //   await I.see('Respondent');
-  // }
-  //
-  // await I.see(labels.factsStated);
-  // await I.see(firstLetterToCaps(verifyContent.D8StatementOfTruth));
-  //
-  // // Reason details
-  // await I.see(labels.reasonForDivorce);
-  // await I.see(reason);
-  //
-  // if(reasonsForDivorce.ADULTERYDISPLAY === reason) {
-  //   await I.see(labels.adulteryDetails);
-  //   await I.see(verifyContent.D8ReasonForDivorceAdulteryDetails);
-  //   await I.see(labels.nameOfCoRespondent);
-  //   await I.see(firstLetterToCaps(verifyContent.D8ReasonForDivorceAdulteryWishToName));
-  //   await I.see(labels.adulteryInfoThirdPartyDetails);
-  //   await I.see(verifyContent.D8ReasonForDivorceAdultery2ndHandDetails);
-  //   await I.see(labels.adulteryPetitionerKnowsWhen);
-  //   await I.see(firstLetterToCaps(verifyContent.D8ReasonForDivorceAdulteryKnowWhen));
-  //   await I.see(labels.detailsOfWhenAdulteryTookPlace);
-  //   await I.see(verifyContent.D8ReasonForDivorceAdulteryWhenDetails);
-  //   await I.see(labels.adulteryPetitionerKnowsWhere);
-  //   await I.see(firstLetterToCaps(verifyContent.D8ReasonForDivorceAdulteryKnowWhere));
-  //   await I.see(labels.detailsOfWhereAdulteryTookPlace);
-  //   await I.see(verifyContent.D8ReasonForDivorceAdulteryWhereDetails);
-  // } else if(reasonsForDivorce.BEHAVIOURDISPLAY === reason) {
-  //   await I.see(labels.behaviourDetails);
-  //   await I.see(verifyContent.D8DerivedStatementOfCase);
-  // } else if (reasonsForDivorce.DESERTIONDISPLAY === reason) {
-  //   await I.see(labels.respondentLeaveWithoutAgreement);
-  //   await I.see(verifyContent.D8ReasonForDivorceDesertionAgreed);
-  //   await I.see(labels.desertionDetails);
-  //   await I.see(verifyContent.D8ReasonForDivorceDesertionDetails);
-  //   await I.see(labels.desertionDate);
-  //   await I.see(formatDateToCcdDisplayDate(new Date(verifyContent.D8ReasonForDivorceDesertionDate)));
-  //   await I.see(labels.livedApart);
-  //   await I.see(verifyContent.D8LivedApartSinceDesertion);
-  // } else if (reasonsForDivorce.SEPFIVEYRSDISPLAY === reason || reasonsForDivorce.SEPTWOYRSDISPLAY === reason ) {
-  //   await I.see(labels.d8MentalSeparationDate);
-  //   await I.see(formatDateToCcdDisplayDate(new Date(verifyContent.D8MentalSeparationDate)));
-  //   await I.see(labels.d8PhysicalSeparationDate);
-  //   await I.see(formatDateToCcdDisplayDate(new Date(verifyContent.D8PhysicalSeparationDate)));
-  //   await I.see(labels.d8ReasonForDivorceSeperationDate);
-  //   await I.see(formatDateToCcdDisplayDate(new Date(verifyContent.D8PhysicalSeparationDate)));
-  //   await I.see(labels.d8LivedApartSinceSeparation);
-  //   await I.see(firstLetterToCaps(verifyContent.D8LivedApartSinceSeparation));
-  // }
-  //
+  await I.see(labels.gender);
+  await I.see(labels.nameChanged);
+  await I.see(labels.divorcingWho);
+  await I.see(labels.marriageBrokenDown);
+  await I.see(labels.phoneNumber);
+  await I.see(labels.emailAddress);
+  await I.see(labels.homeAddress);
+  await I.see(labels.applicantSolicitor);
+  await I.see(labels.solicitorRef);
+  await I.see(labels.solicitorName);
+  await I.see(labels.solicitorPhoneNumber);
+  await I.see(labels.solicitorEmail);
+  await I.see(labels.respondentHeading);
+  await I.see(labels.nameChanged);
+  await I.see(labels.shareRespondentContactDetails);
+  await I.see(labels.respondentSolicitorHeading);
+  await I.see(labels.solicitorName);
+  await I.see(labels.solicitorEmail);
+  //await I.see(labels.solicitorAddress);
+  await I.see(labels.marriageAndCertificate);
+  await I.see(labels.marriagedate);
+  await I.see(labels.applicantNameOnMarriageCert);
+  await I.see(labels.respNameOnMarriageCert);
+  await I.see(labels.sameSexCouple);
+  await I.see(labels.marriageInUK);
+  await I.see(labels.jurisdictionHeading);
+  await I.see(labels.legalConnectionsHeading);
+  await I.see(labels.anyPreviousCourtProceedings);
+  await I.see(labels.doYouWantToApplyForFO);
+  await I.see(labels.isUrgentCase);
+  await I.see(labels.reconcileWithApplicant);
+  await I.see(labels.reconciliationHelp);
+  await I.see(labels.applicantAuthorises);
+  await I.see(labels.yourName);
+  await I.see(labels.firmName);
+  await I.see(labels.additionalComments);
+
+  // AoS, Documents, Service Application , Marriage Certificate
+
   // // Legal Connections
-  // await I.see(labels.legalConnections);
-  // await I.see(verifyContent.D8Connections.A);
-  // await I.see(verifyContent.D8Connections.C);
-  // await I.see(labels.livedTogether);
-  // await I.see(verifyContent.D8DerivedLivingArrangementsLastLivedAddr);
+
+  // other tabs .
 };
--- tests/nfdiv/nightly/verifyServiceDecision.test.js
+++ tests/nfdiv/nightly/verifyServiceDecision.test.js
@@ -2,6 +2,7 @@ const {createNFDCaseInCcd,updateNFDCaseInCcd,updateRoleForCase,shareCaseToRespon
 const { states, events , user, stateDisplayName} = require('../../../common/constants');
 const assert = require('assert');
 const testConfig = require('./../../config');
+const testconfig = require('./../../config');
 
 
 const verifyState = (eventResponse, state) => {
@@ -13,7 +14,7 @@ let caseNumber;
 Feature('NFD Case - Verify Service Decision');
 
 
-Scenario('NFD - Service Application , Service Payment and  Service Decision', async function (I) {
+Scenario('NFD - Divorce Case   - Service Application , Service Payment and  Service Decision', async function (I) {
 
   caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-case.json');
   console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
@@ -50,6 +51,58 @@ Scenario('NFD - Service Application , Service Payment and  Service Decision', as
   await I.checkState(stateDisplayName.AWAITING_SERVICE_CONSIDERATION, events.CONFIRM_SERVICE_PAYMENT);
 
   await I.wait(3);
+  await I.checkNextStepForEvent('Make service decision');
+  await I.submitApproveServiceApplication(caseNumber);
+  await I.submitApproveServiceApplicationCYA(caseNumber);
+  await I.checkState(stateDisplayName.TWENTY_WEEK_HOLDING_PERIOD, events.MAKE_SERVICE_DECISION);
+
+}).retry(testConfig.TestRetryScenarios);
+
+Scenario('NFD - Civil Case     - Service Application , Service Payment and  Service Decision', async function (I) {
+
+  caseNumber = await createNFDCaseInCcd('data/ccd-nfdiv-sole-draft-civil-case.json');
+  //console.log( '..... caseCreated in CCD , caseNumber is ==  ' + caseNumber);
+
+  // SoT solServiceMethod == courtService
+  const awaitingHWF = await updateNFDCaseInCcd(user.SOLS,caseNumber, events.SOLICITOR_SUBMIT_APPLICATION,'data/ccd-nfd-draft-sot-courtservice.json');
+  verifyState(awaitingHWF, states.AWAITING_HWF);
+
+  const hwfAccepted = await updateNFDCaseInCcd(user.CW,caseNumber, events.CASEWORKER_HWF_APPLICATION_ACCEPTED,'data/ccd-nfd-hwf-accepted.json');
+  verifyState(hwfAccepted, states.SUBMITTTED);
+
+  const issueAosPack = await updateNFDCaseInCcd(user.CA,caseNumber, events.ISSUED_FROM_SUBMITTED,'data/ccd-update-place-of-marriage.json');
+  verifyState(issueAosPack, states.AOS_AWAITING);
+
+  await I.amOnHomePage();
+  await I.wait(5);
+  await I.login(testConfig.TestEnvCourtAdminUser, testConfig.TestEnvCourtAdminPassword);
+  await I.wait(3);
+  await I.filterByCaseId(caseNumber);
+  await I.amOnPage('/case-details/' + caseNumber);
+  await I.wait(5);
+  await I.see('AoS awaiting');
+  await I.see('Application issue');
+  await I.checkNextStepForEvent('Service application received');
+
+  await I.submitServiceApplicationReceived(caseNumber);
+  await I.submitServiceApplicationReceivedCYA(caseNumber);
+  await I.checkState(stateDisplayName.AWAITING_SERVICE_PAYMENT, events.SERVICE_APPLICATION_RECEIVED);
+
+  await I.wait(3);
+  await I.checkNextStepForEvent('Confirm service payment');
+  await I.submitServiceApplicationPayment(caseNumber);
+  await I.submitServiceApplicationPaymentCYA(caseNumber);
+  await I.submitServiceApplicationPaymentSubmit(caseNumber);
+  await I.checkState(stateDisplayName.AWAITING_SERVICE_CONSIDERATION, events.CONFIRM_SERVICE_PAYMENT);
+
+  await I.amOnHomePage();
+  await I.wait(7);
+  await I.login(testconfig.TestEnvLegalAdvisorUser, testconfig.TestEnvLegalAdvisorPassword);
+  await I.wait(7);
+  await I.shouldBeOnCaseListPage();
+  await I.wait(5);
+  await I.amOnPage('/case-details/' + caseNumber);
+  await I.wait(5);
   await I.checkNextStepForEvent('Make service decision');
   await I.submitApproveServiceApplication(caseNumber);
   await I.submitApproveServiceApplicationCYA(caseNumber);
