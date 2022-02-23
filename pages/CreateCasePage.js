602765ba288126bc7db5eb5790a3e69fe989daad
Annika <annika.moorthy@solirius.com>
2022-02-23 14:20:28 +0000

    updates to final order

--- pages/CreateCasePage.js
+++ pages/CreateCasePage.js
@@ -12,6 +12,10 @@ module.exports = {
     divorce:'#divorceOrDissolution-divorce',
     dissolution:'#divorceOrDissolution-dissolution',
     event: 'select[id="cc-event"]',
+    stateOption: 'select[id="stateToTransitionApplicationTo"]',
+    applicant1Represented: '#applicant1SolicitorRepresented_Yes',
+    applicant2Represented: '#applicant2SolicitorRepresented_Yes',
+    applicant2ID: '#applicant2UserId',
     submit: 'button[type="submit"]'
   },
 
@@ -34,6 +38,35 @@ module.exports = {
     await I.retry(5).selectOption(this.fields.event, 'Apply: divorce or dissolution');
     await I.waitForNavigationToComplete(this.fields.submit);
     await I.wait(1);
+  },
+
+  async fillTesCaseFormAndSubmit() {
+    if (testConfig.TestForCrossBrowser) {
+      await I.wait(60);
+    } else {
+      await I.wait(5);
+    }
+    await I.waitForText('Family Divorce');
+    await I.retry(5).selectOption(this.fields.jurisdiction, 'Family Divorce');
+    await I.retry(5).selectOption(this.fields.caseType, 'New Law Case');
+    await I.retry(5).selectOption(this.fields.event, 'Create test case');
+    await I.waitForNavigationToComplete(this.fields.submit);
+    await I.wait(1);
+  },
+
+  async fillCreateTestCase() {
+
+    await I.waitForText('Create test case');
+    await I.retry(5).click(this.fields.applicationTypeSole);
+    await I.retry(5).click(this.fields.applicant1Represented);
+    await I.retry(5).click(this.fields.applicant2Represented);
+    await I.retry(5).fillField(this.fields.applicant2ID, 'niwokoruta@mailinator.com');
+    await I.retry(5).selectOption(this.fields.stateOption, 'AwaitingFinalOrder');
+    await I.waitForNavigationToComplete(this.fields.submit);
+  },
+
+  async pressSubmit() {
+    await I.waitForNavigationToComplete(this.fields.submit);
   },
 
   async fillHowDoYouWantToApplyForDivorce(soleOrJoint,divorceOrCivil) {
