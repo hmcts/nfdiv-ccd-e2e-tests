567b23fb6611ce3c83d886ee4f4344d2db6e096a
prabhamuthu15 <praba.muthu@justice.gov.uk>
2022-12-09 12:28:39 +0000

    RemoveNavigationToCompleteMethod

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -13,8 +13,10 @@ module.exports = {
     await I.wait(5);
     await I.fillField(this.fields.email, email);
     await I.fillField(this.fields.password, password);
-    await I.waitForNavigationToComplete(this.fields.submit,testConfig.TestTimeToWaitForText);
-    await I.wait(5);
-    //await I.waitInUrl('/cases',testConfig.TestTimeToWaitForText);
+    await I.click(this.fields.submit);
+    //The waitForNavigationToComplete is not working
+    // await I.waitForNavigationToComplete(this.fields.submit,testConfig.TestTimeToWaitForText);
+    //await I.wait(5);
+    await I.waitInUrl('/cases',testConfig.TestTimeToWaitForText);
   }
 };
