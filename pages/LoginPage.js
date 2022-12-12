f0758ce7b9314e0fc3b0375fbd0297a8fe43e065
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2022-12-12 16:16:12 +0000

    Adding I.waitForText() in the LoginPage.js

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -9,14 +9,13 @@ module.exports = {
     submit: 'input[type="submit"]'
   },
 
-  async submitLogin(email, password) {
+  async   submitLogin(email, password) {
     await I.wait(5);
     await I.fillField(this.fields.email, email);
     await I.fillField(this.fields.password, password);
+    //await I.waitForNavigationToComplete(this.fields.submit,testConfig.TestTimeToWaitForText);
     await I.click(this.fields.submit);
-    //The waitForNavigationToComplete is not working
-    // await I.waitForNavigationToComplete(this.fields.submit,testConfig.TestTimeToWaitForText);
-    //await I.wait(5);
+    await I.waitForText('Case list',testConfig.TestTimeToWaitForText);
     await I.waitInUrl('/cases',testConfig.TestTimeToWaitForText);
   }
 };
